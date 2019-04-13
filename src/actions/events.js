d42688980a96cab616d197e4bcd3ac4919be690a
Bartek Dybowski <bartlomiej.dybowski@goingapp.pl>
2019-04-13 08:31:55 +0200

    comments added

--- src/actions/events.js
+++ src/actions/events.js
@@ -1,7 +1,12 @@
 export const EVENTS_CLEAR = 'EVENTS_CLEAR';
 
+// kreator akcji to zwykła funkcja zwracająca akcję
 export function clearEvents() {
+  // akcja to zwykły obiekt, któr musi zawierać właściwość "type"
+  // może też zawierać inne właściwości, ale o tym później
   return {
+    // typ akcji to zwykły ciąg znaków - zwyczajowo typy przypisuje się do stałych,
+    // ponieważ będą one potrzebne w kilku miejscach w aplikacji
     type: EVENTS_CLEAR,
   };
 }
